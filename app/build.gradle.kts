fdda799ec2c8f735aec9efa0de6659051ec3dc4f
ma7moud3ly <engma7moud3ly@gmail.com>
2026-09-22 15:29:09 +0300

    build: add koin-compose and bump Kotlin, Compose and Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,6 +133,7 @@ dependencies {
     implementation(platform(libs.koin.bom))
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+    implementation(libs.koin.compose)
     implementation(libs.koin.androidx.compose)
     implementation(libs.koin.compose.viewmodel)
     implementation(libs.koin.compose.viewmodel.navigation)
